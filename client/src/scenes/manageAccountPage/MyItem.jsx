38391d59a8530387a2b705612aa8233b73450bcb
vanessa330 <vanessa.33010@gmail.com>
2023-06-08 20:48:57 +0800

    delete unnecessary import

--- client/src/scenes/manageAccountPage/MyItem.jsx
+++ client/src/scenes/manageAccountPage/MyItem.jsx
@@ -37,8 +37,8 @@ const MyItem = ({ id, productName, price, description, picturePath }) => {
 
     if (res.status === 200) {
       window.alert(data.message);
+      window.location.reload();
     }
-    window.location.reload();
   };
 
   return (
