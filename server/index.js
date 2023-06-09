0166495a64e0120317bdc2fe12c2118682ea4671
vanessa330 <vanessa.33010@gmail.com>
2023-06-09 15:21:19 +0800

    updated spinner UI

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   if (req.originalUrl === "/webhook") {
     next();
   } else {
-    express.json();
+    express.json()(req, res, next);
   }
 });
 app.use(express.urlencoded({ extended: true }));
